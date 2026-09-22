7aae5b97b62e2feb10b21cb55e073ccf25bd229b
Dazai <dazaiosamux11@gmail.com>
2026-09-22 23:21:20 +0100

    Completed Python Lessons 1 and 2

--- hello.py
+++ hello.py
@@ -1,1 +1,30 @@
-print("Hello,I am learning Python!")
+#LESSON 1-VARIABLES
+name = "Dazai"
+age = 17
+course = "Mechatronics Engineering"
+print(name)
+print(age)
+print(course)
+
+#LESSON 2-DATA TYPES
+student_name = "Dazai"
+student_age = 17
+cgpa = 3.94
+is_student = True
+print(type("Dazai"))
+print(type(17))
+print(type(3.94))
+print(type(True))
+age = 20
+age_text = "20"
+print(age)
+print(age_text)
+print(type(age))
+print(type(age_text))
+age = "20"
+age = int(age)
+print(age)
+print(type(age))
+price = 500
+price = int(price)
+print(price + 100)
